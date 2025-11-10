fa26928f5e2de4f9c23b8ca759b16a0162438052
pangqingzhu <pangqingzhu2024@gmail.com>
2025-11-10 09:47:13 +0800

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again\n").unwrap();
-    write!(vga_buffer::WRITER.lock(), "some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
